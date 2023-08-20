f70b824239b3f823d8472ba83b216ab0581a1e84
LinkWHorter <LinkWHorter@gmail.com>
2023-08-20 15:18:03 +0300

    Stacks: first repository added :DDDx;

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 function splitNumber() {
+    let input = document.getElementById("number");
+    let value = input.value;
+    if (/^\d+$/.test(value)) {
+      // если введено число, оставляем его без изменений
+      input.value = value;
+    } else if (/^\d+[+]\d+$/.test(value)) {
+      // если введена схема x+y, вычисляем сумму и выводим ее
+      let numbers = value.split("+");
+      let sum = parseInt(numbers[0]) + parseInt(numbers[1]);
+      input.value = sum;
+    } else if (/^\d+[-]\d+$/.test(value)) {
+      // если введена схема x-y, вычисляем разность и выводим ее
+      let numbers = value.split("-");
+      let diff = parseInt(numbers[0]) - parseInt(numbers[1]);
+      input.value = diff;
+    } else if (/^\d+[*]\d+$/.test(value)) {
+      // если введена схема x*y, вычисляем произведение и выводим его
+      let numbers = value.split("*");
+      let prod = parseInt(numbers[0]) * parseInt(numbers[1]);
+      input.value = prod;
+    } else if (/^\d+[/]\d+$/.test(value)) {
+      // если введена схема x/y, вычисляем произведение и выводим его
+      let numbers = value.split("/");
+      let prod = parseInt(numbers[0]) / parseInt(numbers[1]);
+      input.value = prod;
+    }
     var alltd = document.getElementsByTagName("td");
     var st = document.getElementById("stucks");
     var it = document.getElementById("items");
